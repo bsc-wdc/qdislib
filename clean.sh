06e0ae69512acda8d371d401d752ac034b99170b
fconejer <francisco.conejero@bsc.es>
2024-03-04 12:43:27 +0100

    Fixed clean script

--- clean.sh
+++ clean.sh
@@ -8,5 +8,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf "${SCRIPT_DIR}/build"
 rm -rf "${SCRIPT_DIR}/src/qdislib.egg-info"
+rm -rf "${SCRIPT_DIR}/dist"
 
 find "${SCRIPT_DIR}" | grep __pycache__ | xargs rm -rf
