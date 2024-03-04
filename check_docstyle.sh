f10d4d3b0d95a282f6ea81d56faff8aa547fec00
fconejer <francisco.conejero@bsc.es>
2024-03-04 15:45:07 +0100

    Added check scripts.

---
+++ check_docstyle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+# Runs pydocstyle to check the code documentationstyle on the qdislib.
+pydocstyle ./src/
---
+++ check_style.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+# Runs flake8 to check the code style on the qdislib.
+flake8 ./src/
