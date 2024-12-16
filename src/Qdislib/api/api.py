40f20e51f2fb1f580a0ca92b42e2db696f235fdb
mtejedor <mtejedor@bsc.es>
2024-12-16 12:55:03 +0100

    more imports

--- src/Qdislib/api/api.py
+++ src/Qdislib/api/api.py
@@ -24,3 +24,5 @@ from Qdislib.core.optimal_cut.optimal_cut import *
 from Qdislib.core.qubit_mapping.qubit_mapping import *
 from Qdislib.core.graph_algorithms.max_cut import *
 from Qdislib.core.graph_algorithms.random_graph_generator import *
+from Qdislib.utils.circuit import *
+from Qdislib.utils.graph import *
